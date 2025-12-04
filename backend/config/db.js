b659a3f1357292efe6009daad10fec97755127dd
huuthang123 <ngthang0311@gmail.com>
2025-12-04 11:34:04 +0700

    Keep my version, discard incoming changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,6 @@ const config = {
   user: process.env.DB_USER || 'admin_web',
   password: process.env.DB_PASSWORD || '123456',
   server: process.env.DB_SERVER || 'XUANHANH\\CSDL',
-=======
-  user: process.env.DB_USER || 'sa',
-  password: process.env.DB_PASSWORD || 'Thang@123',
-  server: process.env.DB_SERVER || 'DESKTOP-RJSRD5P',
->>>>>>> ae0593a67756b636735aa87496db449960755e2a
   port: parseInt(process.env.DB_PORT) || 1433,
   database: process.env.DB_NAME || 'EcoDB',
   options: {
@@ -21,11 +16,7 @@ const config = {
 const connectDB = async () => {
   try {
     await sql.connect(config);
-<<<<<<< HEAD
     console.log(`✅ Connected to SQL Server (${config.server}:${config.port})`);
-=======
-    console.log('✅ Connected to SQL Server (localhost:1433)');
->>>>>>> ae0593a67756b636735aa87496db449960755e2a
   } catch (error) {
     console.error('❌ Lỗi kết nối SQL Server:', error);
   }
